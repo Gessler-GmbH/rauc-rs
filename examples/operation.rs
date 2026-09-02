bf249f7cc05f87d5c521f8ad701ba9204f893114
Davi Klein Spindola <spindola@gessler.de>
2026-09-02 14:08:55 +0200

    Add rauc version file

--- examples/operation.rs
+++ examples/operation.rs
@@ -1,11 +1,11 @@
-use zbus::{Connection, Result};
-
 use rauc::InstallerProxy;
+use zbus::{Connection, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let connection = Connection::system().await?;
     let proxy = InstallerProxy::new(&connection).await?;
+
     let reply = proxy.operation().await?;
 
     println!("{:?}", reply);
--- tools/generate-interface.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-mkdir -p src/generated
-
-zbus-xmlgen file interfaces/de.pengutronix.rauc.Installer.xml -o src/generated/installer.rs
-# zbus-xmlgen file interfaces/de.pengutronix.rauc.Poller.xml -o src/generated/poller.rs
